4439d9e46e362484668f26c708fb73fefda3f6f0
GLORY8235 <gocityfrontenddevelopment@gmail.com>
2025-04-15 15:31:17 +0100

    / import {franc} from"franc"; // import langs from"langs";

--- langyage_guesser/index.js
+++ langyage_guesser/index.js
@@ -1,5 +1,23 @@
-import {franc} from"franc";
+// import {franc} from"franc";
+// import langs from"langs";
 
-console.log(franc('Alle menslike wesens word vry')); //=> 'afr'
+// const input = process.argv[2];
+// // console.log(franc('Alle menslike wesens word vry')); //=> 'afr'
 
+// const langcode = franc(input);
+// const language = langs.where("3", langcode);
+// console.log(language.name)
  
+import {franc} from "franc";
+import langs from "langs";
+
+const input = process.argv[2]; // Text passed from command line
+
+const langcode = franc(input); // 'afr', 'eng', etc.
+
+if (langcode === 'und') {
+  console.log("Sorry, couldn't guess the language.");
+} else {
+  const language = langs.where("3", langcode);
+  console.log(`Detected language: ${language.name}`);
+}
